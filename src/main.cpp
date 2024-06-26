4870843b77bd114c70e07406d481701e17b5f48c
stein.jonker <stein.jonker@eraze.com>
2024-06-26 19:33:09 +0200

    Set correct calibration factor

--- src/main.cpp
+++ src/main.cpp
@@ -24,32 +24,58 @@
 
 HX711 scale;
 
-//149470 = 229 g
+// A samsung galaxy S21 Ultra weighs 229g and we got a reading of 149470
+// Use .0 at the end of the numbers to force the compiler to use floating point math
+float calibration_factor = 149470.0 / 229.0;
+// On average a can = 12.8g
 
 void setup() {
     Serial.begin(57600);
 
     scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+    Serial.println("Before setting up the scale:");
+    Serial.print("read: \t\t");
+    Serial.println(scale.read());			// print a raw reading from the ADC
+
+    Serial.print("read average: \t\t");
+    Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
+
+    Serial.print("get value: \t\t");
+    Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+    Serial.print("get units: \t\t");
+    Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
+    // by the SCALE parameter (not set yet)
+
+    scale.set_scale(calibration_factor);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+    scale.tare();				                        // reset the scale to 0
+
+    Serial.println("After setting up the scale:");
+
+    Serial.print("read: \t\t");
+    Serial.println(scale.read());                 // print a raw reading from the ADC
+
+    Serial.print("read average: \t\t");
+    Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+    Serial.print("get value: \t\t");
+    Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+    Serial.print("get units: \t\t");
+    Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+    // by the SCALE parameter set with set_scale
+
+    Serial.println("Readings:");
 }
 
 void loop() {
-    if (scale.is_ready()) {
-        scale.set_scale();
-        Serial.println("Tare... remove any weights from the scale.");
-        delay(5000);
-        scale.tare();
-        Serial.println("Tare done...");
-        Serial.print("Place a known weight on the scale...");
-        delay(5000);
-        long reading = scale.get_units(10);
-        Serial.print("Result: ");
-        Serial.println(reading);
-    }
-    else {
-        Serial.println("HX711 not found.");
-    }
+    Serial.print("one reading:\t");
+    Serial.print(scale.get_units(), 1);
+    Serial.print("\t| average:\t");
+    Serial.println(scale.get_units(10), 1);
 
+    scale.power_down();			        // put the ADC in sleep mode
     delay(1000);
+    scale.power_up();
 }
-
-//calibration factor will be the (reading)/(known weight)
