cc091cf3f3d2b89fac3dbbc4b8f941bdf7a96584
stein.jonker <stein@jnkr.eu>
2024-08-28 20:57:09 +0200

    Formatting

--- src/hardware/scale/Scale.cpp
+++ src/hardware/scale/Scale.cpp
@@ -24,13 +24,10 @@ void Scale::setup() {
     Serial.println(scale.read_average(20));    // print the average of 20 readings from the ADC
 
     Serial.print("get value: \t\t");
-    Serial.println(scale.get_value(
-            5));        // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+    Serial.println(scale.get_value(5));        // print the average of 5 readings from the ADC minus the tare weight (not set yet)
 
     Serial.print("get units: \t\t");
-    Serial.println(scale.get_units(5),
-                   1);    // print the average of 5 readings from the ADC minus tare weight (not set) divided
-    // by the SCALE parameter (not set yet)
+    Serial.println(scale.get_units(5),1);    // print the average of 5 readings from the ADC minus tare weight (not set) divided by the SCALE parameter (not set yet)
 
     scale.set_scale(calibration_factor);                      // this value is obtained by calibrating the scale with known weights; see the README for details
     scale.tare();                                        // reset the scale to 0
@@ -44,12 +41,10 @@ void Scale::setup() {
     Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
 
     Serial.print("get value: \t\t");
-    Serial.println(scale.get_value(
-            5));        // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+    Serial.println(scale.get_value(5));        // print the average of 5 readings from the ADC minus the tare weight, set with tare()
 
     Serial.print("get units: \t\t");
-    Serial.println(scale.get_units(5),
-                   1);        // print the average of 5 readings from the ADC minus tare weight, divided
+    Serial.println(scale.get_units(5),1);        // print the average of 5 readings from the ADC minus tare weight, divided
     // by the SCALE parameter set with set_scale
 
     Serial.println("Scale Setup Complete");
